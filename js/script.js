e832f48ce0593dff87d94cc60319595676ab646e
William Mata <william.xavante@gmail.com>
2022-04-18 20:34:31 -0300

    Index e Sobre
    Index e sobre concluidos

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var div = document.getElementById('subTitulo');
+var texts = ['Seja Bem-vindo(a)!', 'Desenvolvedor Full Stack', 'Desenvolver é Arte', 'Desenvolver é Inovar', 'Desenvolver é Essencial' , 'Desenvolver é Solucionar Problemas'];
+
+function writerText(str, done) {
+    var char = str.split('').reverse();
+    var typer = setInterval(() => {
+        if (!char.length) {
+            clearInterval(typer);
+            return setTimeout(done, 500); // só para esperar um bocadinho
+        }
+        var next = char.pop();
+        div.innerHTML += next;
+    }, 100);
+}
+
+function cleanText(done) {
+    var char = div.innerHTML;
+    var nr = char.length;
+    var typer = setInterval(() => {
+        if (nr-- == 0) {
+            clearInterval(typer);
+            return done();
+        }
+        div.innerHTML = char.slice(0, nr);
+    }, 100);
+}
+
+function start(conteudos) {
+    var now = -1;
+    function next(){
+        if (now < conteudos.length - 1) now++;
+        else now = 0;
+        var str = conteudos[now];
+        writerText(str, function(){
+            cleanText(next);
+        });
+    }
+    next(next);
+}
+
+start(texts);
