4989f09d77effdb3433390e0736cca13fcb0e559
William Mata <william.xavante@gmail.com>
2022-04-18 22:10:48 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,41 +1,62 @@
-var div = document.getElementById('subTitulo');
-var texts = ['Seja Bem-vindo(a)!', 'Desenvolvedor Full Stack', 'Desenvolver é Arte', 'Desenvolver é Inovar', 'Desenvolver é Essencial' , 'Desenvolver é Solucionar Problemas'];
+var nome = document.getElementById('nome');
+var subTitulo = document.getElementById('subTitulo');
+var texts = ['Seja Bem-vindo(a)!', 'Desenvolvedor Full Stack', 'Desenvolver é Arte', 'Desenvolver é Inovar', 'Desenvolver é Essencial', 'Desenvolver é Solucionar Problemas De Forma Inteligente'];
+
+
+window.onload = () => {
+    document.getElementById('text').style.display = "flex";
+    var text = "WILLIAM DE MATA DA SILVA".split('');
+    text.forEach((letra, w) => {
+        setTimeout(() => {
+            nome.innerHTML += letra;
+
+        }, 50 * w)
+    })
+
+
+
+}
 
 function writerText(str, done) {
     var char = str.split('').reverse();
     var typer = setInterval(() => {
         if (!char.length) {
             clearInterval(typer);
-            return setTimeout(done, 500); // só para esperar um bocadinho
+            return setTimeout(done, 500); 
         }
         var next = char.pop();
-        div.innerHTML += next;
+        subTitulo.innerHTML += next;
     }, 100);
 }
 
 function cleanText(done) {
-    var char = div.innerHTML;
+    var char = subTitulo.innerHTML;
     var nr = char.length;
     var typer = setInterval(() => {
         if (nr-- == 0) {
             clearInterval(typer);
             return done();
         }
-        div.innerHTML = char.slice(0, nr);
+        subTitulo.innerHTML = char.slice(0, nr);
     }, 100);
 }
 
 function start(conteudos) {
+    
     var now = -1;
-    function next(){
+    function next() {
         if (now < conteudos.length - 1) now++;
         else now = 0;
         var str = conteudos[now];
-        writerText(str, function(){
+        writerText(str, function () {
             cleanText(next);
         });
     }
     next(next);
+    
 }
 
-start(texts);
+setTimeout(() => {
+    document.getElementById('text').style.display = "inline-block";
+    start(texts);
+}, 1200)
